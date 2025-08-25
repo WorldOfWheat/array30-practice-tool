9a8af983358c1b693b94c2315d3485787128e68c
WorldOfWheat <a302854888@proton.me>
2025-08-25 19:22:14 +0800

    feat: Add table parsing utility functions for fetching and processing data from text files

---
+++ src/utils/tableParser.ts
@@ -0,0 +1,79 @@
+function getKey(line: string): string {
+  return line.split("\t")[1].replace("\r", "").replace("\n", "");
+}
+
+function getValue(line: string, enterKey: string): string {
+  return line.split("\t")[0] + enterKey;
+}
+function handleTabSplitTable(lines: string[]): Map<string, string[]> {
+  const table = new Map<string, string[]>();
+
+  function addToTable(key: string, value: string) {
+    if (table.has(key)) {
+      table.get(key)?.push(value);
+    } else {
+      table.set(key, [value]);
+    }
+  }
+
+  let characterCounter = 1;
+  let lastChar = "";
+  for (const line of lines) {
+    if (line.trim() === "") continue; // skip empty lines
+    if (lastChar === "") {
+      lastChar = line.split("\t")[0];
+      const key = getKey(line);
+      const value = getValue(line, (characterCounter % 10).toString());
+      addToTable(key, value);
+    } else {
+      const character = line.split("\t")[0];
+      if (character === lastChar) {
+        characterCounter++;
+      } else {
+        characterCounter = 1;
+      }
+
+      const key = getKey(line);
+      const value = getValue(line, (characterCounter % 10).toString());
+      addToTable(key, value);
+      lastChar = character;
+    }
+  }
+
+  return table;
+}
+
+export async function getFirstLevelTable(): Promise<Map<string, string[]>> {
+  const file = await fetch("/1.txt");
+  const text = await file.text();
+  const lines = text.split("\n");
+  return handleTabSplitTable(lines);
+}
+
+export async function getSecondLevelTable(): Promise<Map<string, string[]>> {
+  const file = await fetch("/2.txt");
+  const text = await file.text();
+  const lines = text.split("\n");
+  return handleTabSplitTable(lines);
+}
+
+export async function getSpecialCharTable(): Promise<Map<string, string[]>> {
+  const table = new Map<string, string[]>();
+  const file = await fetch("/3.txt");
+  const text = await file.text();
+  const lines = text.split("\n");
+  for (const line of lines) {
+    if (line.trim() === "") continue; // skip empty lines
+    const key = getKey(line);
+    const value = getValue(line, ' ');
+    table.set(key, [value]);
+  }
+  return table;
+}
+
+export async function getNormalCharTable(): Promise<Map<string, string[]>> {
+  const file = await fetch("/4.txt");
+  const text = await file.text();
+  const lines = text.split("\n");
+  return handleTabSplitTable(lines);
+}
