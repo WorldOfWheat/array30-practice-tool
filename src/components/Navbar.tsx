645e79d670cdd53debf103c3da85879351ec0d5b
WorldOfWheat <a302854888@proton.me>
2025-08-27 01:25:06 +0800

    feat: Implement dark mode toggle in Navbar component with Bootstrap integration

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,30 @@
 import Link from "next/link";
 import styles from "./Navbar.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { useEffect, useState } from "react";
 
-export interface NavbarProps { 
+export interface NavbarProps {
   tab: 'home' | 'wordPractice' | 'articlePractice' | 'about';
 }
 
 export default function Navbar({ tab }: NavbarProps) {
+  const [darkMode, setDarkMode] = useState(() => {
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem('darkMode') === 'true';
+    }
+    return false;
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+    localStorage.setItem('darkMode', darkMode ? 'true' : 'false');
+  }, [darkMode])
+
   return (
     <nav className={`navbar navbar-expand-lg ${styles['navbar']}`}>
       <div className="container-fluid">
@@ -25,6 +44,14 @@ export default function Navbar({ tab }: NavbarProps) {
             </li>
           </ul>
         </div>
+        <span onClick={(() => { setDarkMode(!darkMode); })} className={styles['dark-mode-toggle']}>
+          {!darkMode ?
+            <>
+              <i className="bi bi-brightness-high"></i>
+            </> : <>
+              <i className="bi bi-moon"></i>
+            </>}
+        </span>
       </div>
     </nav>
   )
